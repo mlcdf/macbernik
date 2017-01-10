3975bafedb1e1e9460ade2ab41dcd1dddedfe951
llarousserie <llarousserie@sqli.com>
2017-01-10 17:19:58 +0100

    enabled test framework

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,5 +2,4 @@
 $(document).ready(function()
 {
     var TH_Core = $('#MB').MB_Core();
-    TH_Core.foo_public_method();
 });
--- test/spec/testMB_Core.js
+++ test/spec/testMB_Core.js
@@ -1,3 +1,13 @@
-/**
- * Created by hikingyo on 10/01/17.
- */
+(function () {
+    'use strict';
+
+    let MB_Core = $().MB_Core();
+
+    describe('MB_Core plugin', function () {
+        describe('Construct', function () {
+            it('should have a default settings', function () {
+                expect(MB_Core.settings.foo).to.equal('bar');
+            });
+        });
+    });
+})();
